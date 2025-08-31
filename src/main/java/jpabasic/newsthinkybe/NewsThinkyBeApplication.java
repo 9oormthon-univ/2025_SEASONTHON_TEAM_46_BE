7c293f7306cbb3ef6a249896d3c525b4267b7d7c
minsikk <131092169+1224kang@users.noreply.github.com>
2025-09-01 02:20:51 +0900

    [feat] spring 기본 세팅

---
+++ src/main/java/jpabasic/newsthinkybe/NewsThinkyBeApplication.java
@@ -0,0 +1,13 @@
+package jpabasic.newsthinkybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsThinkyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsThinkyBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpabasic/newsthinkybe/NewsThinkyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabasic.newsthinkybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsThinkyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
